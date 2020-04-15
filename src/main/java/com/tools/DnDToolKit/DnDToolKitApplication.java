aa68feb5f359b9974e98abe28f0482dabab0f73e
Michael Moses <michael-moses@outlook.com>
2020-04-15 17:43:39 -0400

    Inital Commit

---
+++ src/main/java/com/tools/DnDToolKit/DnDToolKitApplication.java
@@ -0,0 +1,13 @@
+package com.tools.DnDToolKit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDToolKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDToolKitApplication.class, args);
+	}
+
+}
