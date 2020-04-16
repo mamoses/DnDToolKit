fe933407505649ee53bbf2d1dc800746f43c7dab
Michael Moses <michael-moses@outlook.com>
2020-04-16 19:24:23 -0400

    Updated Player Controller and home page.

--- src/main/java/com/tools/DnDToolKit/web/PlayerController.java
+++ src/main/java/com/tools/DnDToolKit/web/PlayerController.java
@@ -1,5 +1,26 @@
 package com.tools.DnDToolKit.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tools.DnDToolKit.model.PlayerRepository;
+
+@Controller
 public class PlayerController {
+	
+	private final PlayerRepository playerRepository;
+	
+	 @Autowired
+	 public PlayerController(PlayerRepository playerRepository) {
+	        this.playerRepository = playerRepository;
+	 }
+	 
+	 @GetMapping("/")
+	 public String showHome() {
+		 
+		 
+		 return "home";
+	 }
 
 }
