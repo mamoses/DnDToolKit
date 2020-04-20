e211801a41e91728c2a6b208382c2c4f7686b281
Michael Moses <michael-moses@outlook.com>
2020-04-20 19:05:14 -0400

    Added Navigation bar and finished functions of Initiative Tracker.

--- src/main/java/com/tools/DnDToolKit/web/PlayerController.java
+++ src/main/java/com/tools/DnDToolKit/web/PlayerController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 import com.tools.DnDToolKit.model.Player;
 import com.tools.DnDToolKit.model.PlayerRepository;
@@ -35,9 +37,15 @@ public class PlayerController {
 		 
 		 Sort sort = Sort.by(
 	        	    Sort.Order.desc("initiative"));
-		 
 		 model.addAttribute("players", playerRepository.findAll(sort));
+		 return "initiative";
+	 }
+	 @PostMapping("/initiative")
+	 public String showInitiativePost(@Valid Player player, BindingResult result, Model model){
 		 
+		 Sort sort = Sort.by(
+	        	    Sort.Order.desc("initiative"));
+		 model.addAttribute("players", playerRepository.findAll(sort));
 		 return "initiative";
 	 }
 	
@@ -53,7 +61,6 @@ public class PlayerController {
 		 }
 		 	playerRepository.save(player);
 	        model.addAttribute("players", playerRepository.findAll());
-		 
 		 return "redirect:/initiative";
 	 }
 	 
@@ -64,8 +71,33 @@ public class PlayerController {
 	        }
 		 
 	     model.addAttribute("players", playerRepository.findAll());
-		 
 		 return "add-player";
 	 }
+	 
+	 @GetMapping("/delete/{id}")
+	    public String deletePlayer(@PathVariable("id") long id, Model model) {
+	        Player player = playerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	        playerRepository.delete(player);
+	        model.addAttribute("players", playerRepository.findAll());
+	        return "redirect:/initiative";
+	    }
+	 
+	 @GetMapping("/edit/{id}")
+	    public String editPlayer(@PathVariable("id") long id, Model model) {
+	        Player player = playerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	        model.addAttribute("player", player);
+	        return "edit-player";
+	    }
+	 
+	 @PostMapping("/update/{id}")
+	    public String updatePlayer(@PathVariable("id") long id, @Valid Player player, Model model, BindingResult result) {
+		 if (result.hasErrors()) {
+	            player.setId(id);
+	            return "edit-player";
+	        }
+	        playerRepository.save(player);
+	        model.addAttribute("users", playerRepository.findAll());
+	        return "redirect:/initiative";
+	    }
 
 }
